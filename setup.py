43a44f4eb60e2462dad5d84b71a1262bcf2fa030
Karl Hendrik Nurmeots <karl.nurmeots@gmail.com>
2021-04-03 14:39:54 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ML Playground",
+    version="0.0.1",
+    author="Karl Hendrik Nurmeots",
+    author_email="",
+    description="Implementing machine learning and statistical models from scratch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'jax',
+        'pandas',
+        'matplotlib',
+        'torch'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
